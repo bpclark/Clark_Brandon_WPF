27d41acd0779ce49d132dd7476afdab5219fd770
brandon clark <bpclark@fullsail.edu>
2014-01-13 20:58:19 -0500

    Learning expressions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,19 +1,23 @@
 //JavaScript
-
+/*
 var yearBorn = 1955;
 
-/*
 console.log("Hello World");
 console.log(yearBorn);
-*/
 
 alert(yearBorn);
+*/
 
 //Expressions
-
+/*
 var a = 2; //sets up our varible a defines it with 2
 a = a + 3 //add 3 to a
 console.log(a);
 
+//Expressions - Age Example
+var yearBorn = 1985;
+var age = 2014 - yearBorn - 1;
+console.log(age);
+*/
 
 
