138caff6e4289bd131a116c929489b3f7abef2be
brandon clark <bpclark@fullsail.edu>
2014-01-13 21:07:10 -0500

    Expressions - Arithmetic Operators

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -19,5 +19,14 @@ var yearBorn = 1985;
 var age = 2014 - yearBorn - 1;
 console.log(age);
 */
+/*
+//Expressions - Arithmetic Operators
 
+//area of the triangle is half of the width times the height
+
+var width = 7;
+var height = 8;
+var area = width * height/2;
+console.log(area);
+    */
 
