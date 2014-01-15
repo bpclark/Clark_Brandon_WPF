ad6c574423f7b68d0d5d679ccee966a43fdfcc10
brandon clark <bpclark@fullsail.edu>
2014-01-14 21:00:36 -0500

    Slice of the pie  varibels entered and commented.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,6 +4,26 @@
 
 
 //Dog Years
-var sparkyAge = 4; // declaration and definition
+//Givens: Sparkyes age 1, 7 years human years to 1 dog year
+//Result varible is human years (7) * dog years
 
+var sparkyAge = 1; // declaration and definition
+var humanYears = 7; // declaration and definition
+
+var x = sparkyAge * humanYears; //declaring the dogs age times human years
+var resultX = (x); // needed to make a container for the result of sparkyAge times humanYears
+
+var answer = "Sparky is" + ' ' + (x) + ' ' + "human years old which is" + ' ' + (sparkyAge) + ' ' + "in dog years."  // this expression had to be broken up into strings with spaces added to allow for a structed sentecnce.
+
+//console.log (answer)
+
+
+
+//Slice of the Pie 1
+// Givens: Number of slices 7, People at the party 32, Number of pizzas ordered 12.
+//Result varible  2.625 slices per person.
+
+var slices = 7 //7 slices per box
+var people = 32 //32 people are attending the party
+var ordered = 12 // 12 pizzas have been ordered
 
