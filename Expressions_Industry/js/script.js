54e7c59604e44e87081c7a3edc65be0566a771be
brandon clark <bpclark@fullsail.edu>
2014-01-15 23:54:19 -0500

    calculation hoursComplete*costEmployee*peopleWorking

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,3 +9,5 @@ var peopleWorking = prompt ("This is a calculator to see how much it costs to ac
 var hoursComplete = prompt ("Please enter the projected hours to complete the task at hand.");// defining prompt with planned hours it would take to complete the task.
 
 var costEmployee = prompt ("Now enter the base pay average of the employees");//defining prompt to enter base pay average to help with determining the final fast of man power hours.
+
+var totalCost= (hoursComplete*costEmployee*peopleWorking);
