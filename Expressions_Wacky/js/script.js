978e6409164ab75bbdc01a7765a777a460cf9091
brandon clark <bpclark@fullsail.edu>
2014-01-16 01:20:49 -0500

    prompt for inserting how far you ran

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,4 +6,4 @@
 
 var a=.653; // rate of burn for fat while running.
 
-var distance= prompt ("This calculator allows for measuring how many calories you burned while running. **Enter in how far you ran in miles**");
+var distance= prompt ("This calculator allows for measuring how many calories you burned while running. **Enter in how far you ran in miles**");// prompt for inserting how far you ran
