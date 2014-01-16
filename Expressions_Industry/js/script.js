47a25cbd705b9a5139471eeb2b2b1fa558dc1e33
brandon clark <bpclark@fullsail.edu>
2014-01-15 23:36:25 -0500

    definition of peopleWorking for a prompt

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,6 @@
 //Expressions - Expressions_Industry
 
 //Givens: How many people are working, how long does it take to complete the task, cost of each employee
+
+var peopleWorking = prompt ("This is a calculator to see how much it costs to accomplish a task. Please enter how many people will be working on the task.");
+
