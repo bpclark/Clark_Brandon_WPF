11dd12f65387346c09276a60f445e544f4289268
brandon clark <bpclark@fullsail.edu>
2014-01-16 01:21:53 -0500

    enter in your weight in the prompt

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,4 +8,4 @@ var a=.653; // rate of burn for fat while running.
 
 var distance= prompt ("This calculator allows for measuring how many calories you burned while running. **Enter in how far you ran in miles**");// prompt for inserting how far you ran
 
-var weight= prompt ("Enter your weight")
+var weight= prompt ("Enter your weight")// enter in your weight in the prompt
