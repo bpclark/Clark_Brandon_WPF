2e87fef496e0440fc4ebe526235066943c0ef586
brandon clark <bpclark@fullsail.edu>
2014-01-15 22:04:06 -0500

    added the givens

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,3 +2,9 @@
 //1-15-2014
 //Expressions - Personal - How much it costs to drive to work.
 
+//Givens
+// How many gallons of gas does it take to get to work.
+//Miles driven to work
+// MPG of Car
+
+
