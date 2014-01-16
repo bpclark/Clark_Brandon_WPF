12a3d152484a43f5f70fa642854047383aa60226
brandon clark <bpclark@fullsail.edu>
2014-01-16 01:22:46 -0500

    first calculation distance ran times your wieght  then times .653

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -9,3 +9,5 @@ var a=.653; // rate of burn for fat while running.
 var distance= prompt ("This calculator allows for measuring how many calories you burned while running. **Enter in how far you ran in miles**");// prompt for inserting how far you ran
 
 var weight= prompt ("Enter your weight")// enter in your weight in the prompt
+
+var result= (distance* weight*a)
