438fbaf87b736d5f8ba26f11f9fe835f079ef432
brandon clark <bpclark@fullsail.edu>
2014-01-15 22:24:39 -0500

    expressions to solve MPG/miles to work to give me gallons used

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,4 +7,11 @@
 //Miles driven to work
 // MPG of Car
 
+var gasPrice = prompt ("Enter how much a gallon of gas costs"); //definitions
+var milesDriven = prompt ("How man miles to work do you drive?");//definition
+var mpgCar= prompt ("Enter the MPG of your car");//definition
 
+var gallonsUsed = (milesDriven / mpgCar)* gasPrice; //expressions to solve MPG/miles to work to give me gallons used.
+
+
+console.log(totalCost);
