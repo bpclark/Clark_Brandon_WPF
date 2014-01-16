4ad934efe90199c805696b721b3747521b6e6793
brandon clark <bpclark@fullsail.edu>
2014-01-16 01:18:11 -0500

    adjusted the givens; calories burned , distance ran, weight

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,9 +2,5 @@
 //1-15-2014
 //Expressions - Expressions_Wacky
 
-//Given; Calories for men and woman, calories burned , distance ran, weight
-
-var a=124;
-
-var b=105;
+//Given; calories burned , distance ran, weight
 
