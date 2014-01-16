486af14bfa96fac5810e489efb9f43c33358238c
brandon clark <bpclark@fullsail.edu>
2014-01-15 23:12:00 -0500

    toatl cost is calculated here.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -14,3 +14,4 @@ var mpgCar= prompt ("Enter the MPG of your car");//definition
 var dayGas = prompt (" Enter how many days a month you drive to work ")//definition
 var gallonsUsed = (milesDriven / mpgCar)* (gasPrice);//expressions to solve MPG/miles to work to give me gallons used.
 var perDay = (gallonsUsed * 2)*(dayGas);//definition
+var totalCost = "The total cost to drive one way to work is $" + (perDay.toFixed(2)) + "." 
