98e53f649969aca910a70165f7a1b9ef0845d2fb
brandon clark <bpclark@fullsail.edu>
2014-01-15 23:50:13 -0500

    defining the prompt with how many people would be working on this task.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,4 +4,4 @@
 
 //Givens: How many people are working, how long does it take to complete the task, cost of each employee
 
-var peopleWorking = prompt ("This is a calculator to see how much it costs to accomplish a task. Please enter how many people will be working on the task.");
+var peopleWorking = prompt ("This is a calculator to see how much it costs to accomplish a task. Please enter how many people will be working on the task.");// defining the prompt with how many people would be working on this task.
