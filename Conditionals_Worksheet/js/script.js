9d93ee527668d852b37c9470c9a22e4b3321fac2
brandon clark <bpclark@fullsail.edu>
2014-01-21 17:42:57 -0500

    The givens added

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -6,3 +6,7 @@ Conditionals Worksheet
 Celsius to Fahrenheit converter
 
 Do: Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered. If the user puts a “C” for the unit, the calculator should convert to Celsius. If the user puts “F” for the unit, the calculator should convert to Fahrenheit.
+
+ Given:
+ Degrees (in F or C)
+ Unit (a string holding an “F” or a “C”)
