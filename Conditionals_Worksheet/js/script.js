1f71d23034fe7fb4e87333c07c0639d5fa8a0ab2
brandon clark <bpclark@fullsail.edu>
2014-01-21 17:44:49 -0500

    Data sets to test

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -13,3 +13,11 @@ Unit (a string holding an “F” or a “C”)
 
  Result:
  “The temperature is X degrees Celsius.” Or “The temperature is X degrees Fahrenheit.”
+
+Data Sets to Test: (Note that data sets are not the only numbers that should work with your
+code.)
+
+32F is 0C
+100C is 212F
+90F is 32.22C
+*/
