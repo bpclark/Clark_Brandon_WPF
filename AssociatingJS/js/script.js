a0a1d168cb2a80ffdbf0af2870111afa1cd5cc3d
brandon clark <bpclark@fullsail.edu>
2014-01-21 16:48:31 -0500

    add learned stuff

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -93,9 +93,9 @@ console.log(phoneNo);
 
 //Expressions - Assignment Operators
 
-var a=3;
-a -- ; //a= a+4// a +=1 a= -- 1
-console.log(a);
+//var a=3;
+//a -- ; //a= a+4// a +=1 a= -- 1
+//console.log(a);
 
 
 //prompt and alert
@@ -104,36 +104,140 @@ console.log(a);
 
 //console.log(userInput);
 
-var width = prompt ("We are calculating the area of a rectangle. \nPlease enter the width")
-var height = prompt ("Please enter the height");
-var area= width*height;
+//var width = prompt ("We are calculating the area of a rectangle. \nPlease enter the width")
+//var height = prompt ("Please enter the height");
+//var area= width*height;
 //console.log(area);
-var result = "The area of your rectangle is " + area + " sq feet";
-alert(result);
+//var result = "The area of your rectangle is " + area + " sq feet";
+//alert(result);
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//Conditionals
+
+//if(sunny){
+  //  goToTheBeach();
+//}//else{
+  //  goToTheMovies();
+//}
+
+/*
+var kidHeight= 36;
+var minHeight= 48;
 
 
+//if the child old enough print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
 
+    // code preformed if condition is true
+    console.log ("You can ride the coaster!")
 
+}else{
+    // code preformed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!!")
+}
+*/
+///////////////////////////
+//Conditional Logic - Else - IF
+/*
+var kidHeight= 47;
+var minHeight= 48;
+var wParentHeight = 45;// the of the kid with the parent
 
+//if the child old enough print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
 
+    // code preformed if condition is true
+    console.log ("You can ride the coaster!");
 
+}else if (kidHeight > wParentHeight){
+    // you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+    //sorry you have growing to do
+    console.log("Sorry kid, you've got some growing to do first!!");
+}
+*/
 
+//Conditional Logic - logical Operators
+/*
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
 
+//if the price of the phone is less our budget AND if our paycheck is over 300
 
+if(iPhonePrice < budget && payCheck > 300){
+   console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!");
+}
+*/
+////////////////////////////////
+//OR operator
+/*
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
 
+//if the price of the phone is less our budget AND if our paycheck is over 300
 
+if(iPhonePrice < budget || wonLottery ){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!");
+}
+*/
 
+//Conditional Logic - Ternary Operators
+/*
+if (conditional){
+    do if true;
 
+}else{
+    do if false;
+}
+//Writting the above in one line
+(condition) ? do if true : do if false;
+*/
 
+/*
+var gpa = 48;
 
+//if the gpa is over the 2.0 score then the student can graduate
 
+/*if (gpa > 2.0 ){
+    console.log("You can Graduate!");
+}else{
+    console.log("GPA is too low")
+}*/
+/*
+    (gpa > 2.0)  ? console.log("You can Graduate!") : console.log("GPA is too low");
+*/
 
+////////////////////////////
 
+//Conditional Logic - Ternary Operators
+/*
+var age = 11
+var book;
 
+// if the child is under 10, then they get Green Eggs and Ham, otherwise the get the Time Machine
+if(age<10){
+    book= "Green Eggs and Ham";
+}else{
+    book= "The Time Machine";
+}
+console.log(book);
+*/
 
+var age= 6;
+var book;
 
+book = (age < 12) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
 
 
 
