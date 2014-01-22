16a128f53f82f76f047a7c84c59d7236e124f546
brandon clark <bpclark@fullsail.edu>
2014-01-21 22:44:25 -0500

    the do's: The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price. Determine which of the two prices the customer is eligible for.

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -72,5 +72,5 @@ if (userName == userPop && thePass == enterPass) {//if all 4 conditions are equa
     /*
 
 //Movie Ticket Price
-
-    
+//Do:The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
+ Determine which of the two prices the customer is eligible for.
