73cb7adae6fc8967897918bad7ef541db0e75121
brandon clark <bpclark@fullsail.edu>
2014-01-22 21:07:43 -0500

    print: Make better decisions with your money and get more than what you can afford! You are in the red $X or "Good job! You have $X and are in the black. Would you like a loan?")

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,11 @@
 //Brandon Clark
 //1-22-2014
 //Conditionals
+
+//Given: what is hourly pay, how many hours did you work this month, How much are your bills, how much money is left after bills
+
+//result print: Make better decisions with your money and get more than what you can afford! You are in the red $X or "Good job! You have $X and are in the black. Would you like a loan?")
+
+
+
+
