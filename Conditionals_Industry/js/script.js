9a352bbbb6bba003a53e32f064677638fae1cf17
brandon clark <bpclark@fullsail.edu>
2014-01-22 22:25:10 -0500

    Comment

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,4 +7,4 @@
 
 var rpms = prompt("Enter current max rpms");//prompt for user rpm value
 var cht= prompt ("Enter in the current CHT(cylinder head temp) in celsiuses");//prompt for user to enter the current cht
-var DA= prompt ("Enter the current Density Altitude");
+var DA= prompt ("Enter the current Density Altitude");//prompt for user to enter DA
