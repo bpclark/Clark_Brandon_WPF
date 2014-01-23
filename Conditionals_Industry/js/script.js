e6cf9e1f08a3fe57bd0d4930f30862ad9eb2cc43
brandon clark <bpclark@fullsail.edu>
2014-01-22 22:26:46 -0500

    print to console

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,4 +9,5 @@ var rpms = prompt("Enter current max rpms");//prompt for user rpm value
 var cht= prompt ("Enter in the current CHT(cylinder head temp) in celsiuses");//prompt for user to enter the current cht
 var DA= prompt ("Enter the current Density Altitude");//prompt for user to enter DA
 
-if (rpms >=5400 && rpms <=5600 && cht >=80 && cht <=155 && DA >=0 && DA <3000){
+if (rpms >=5400 && rpms <=5600 && cht >=80 && cht <=155 && DA >=0 && DA <3000){//if the Ternary condtions are met then a go for launch will print
+    console.log ("You are cleared for take off!");
