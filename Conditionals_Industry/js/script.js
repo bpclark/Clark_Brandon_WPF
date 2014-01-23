61cbb65a876fba4b07e9fdb05b4c91f42e6293a4
brandon clark <bpclark@fullsail.edu>
2014-01-22 22:26:21 -0500

    if the Ternary condtions are met then a go for launch will print

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -8,3 +8,5 @@
 var rpms = prompt("Enter current max rpms");//prompt for user rpm value
 var cht= prompt ("Enter in the current CHT(cylinder head temp) in celsiuses");//prompt for user to enter the current cht
 var DA= prompt ("Enter the current Density Altitude");//prompt for user to enter DA
+
+if (rpms >=5400 && rpms <=5600 && cht >=80 && cht <=155 && DA >=0 && DA <3000){
