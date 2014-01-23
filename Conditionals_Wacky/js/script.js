3d9de048eba0d983cdcc8847d516c4e1dec83bfa
brandon clark <bpclark@fullsail.edu>
2014-01-23 00:09:33 -0500

    time of when the mogwai was fed in 24 hour format

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,9 @@
-alert("This is in the js file");
+//Brandon Clark
+//1-22-2014
+//Conditionals_Wacky
+
+//Givens: When can gremlins eat, how many gremlins spawn when per once of water
+//Result: Woot you can follow directions!, EEEWWW! they are gross looking now!,They are multiplying!! Its X of those little guys, Yep we are are going to die! They multiplied and changed !! There is X of them
+
+var gremEat = prompt("When did you feed the mogwai, in 24 hour format please");
+
