563ff176ecf18aabaae5b46bda87b69af328b132
brandon clark <bpclark@fullsail.edu>
2014-01-22 21:11:40 -0500

    prompt for hours worked

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,6 +8,7 @@
 
 var myPay= prompt ("What is your hourly pay?");//prompt for hourly pay
 var myBills= prompt ("How much are your bills monthly?");//prompt for your bills monthly
+var myHours=prompt("How many hours did you work this month?");
 
 
 
