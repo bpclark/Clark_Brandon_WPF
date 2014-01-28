9af93c0122d2a335c1c401aa75327019d0dd4f07
brandon clark <bpclark@fullsail.edu>
2014-01-27 21:37:40 -0500

    creating the anonymous line that is kinda ignored at the begining.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -29,3 +29,5 @@ console.log("The circumference of the circle is " + total + ".");//print to cons
 //Parameter(s) for function: Victim’s weight (in pounds)
 //Return: Number of Bee stings
 //Result to print to the console: “It takes X bee stings to kill this animal.
+
+var calcDeath= function (weight, stings){
