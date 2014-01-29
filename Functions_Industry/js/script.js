b0a7db6289d0aadb75a47a46b598084e623a0125
brandon clark <bpclark@fullsail.edu>
2014-01-28 21:11:08 -0500

    added anonymous functions

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -8,19 +8,23 @@ Givens: Monitor height, width, number of monitors
 Result: 3 statements depending on resolution.
  */
 
-function calcRez(){//normal named function
+var calcRez = function (if1, if2, if3 ){//anonymous named function
     var width = prompt ("Enter a single monitors width in pixels (i.e. 1920 1024 ect.)");//width prompt from user
     var height = prompt ("Enter a single monitors height in pixels.(i.e. 480, 720, 1080)");// height prompt from user
     var maxMon = prompt ("Enter in how many monitors you want set up to share a desktop.");// number of monitors the user will be using.
 
     var maxRez = width* maxMon;// calculation for the maximum screen resolution per added monitor.
 
-    if (maxRez < 1920){//starting the if condition
+    if (maxRez <=if1){//starting the if condition
         console.log("Your screen resolution with " + maxMon + " monitor(s) would be "+ maxRez + " X " + height +"." +  " Not impressed" );//print to console
 
-    }else if (maxRez >= 1920 && maxRez < 3840 ){//else if with logical operators
+    }else if (maxRez >if2 && maxRez < if3 ){//else if with logical operators
         console.log("Your screen resolution with " + maxMon +  " monitor(s) would be " + maxRez + " X " + height + "." + " Go big or go home! Try again when your ready to play with the big dogs!");//print to console
 
     }else {//starting of an else condition of the others have met those criteria.
-        console.log("Your screen resolution with " + maxMon + " monitor(s) would be "+ maxRez + " X " + height + " Now that's what I'm talking about! Welcome to the nerd tech club! WOOTZ WOOTZ!")
+        console.log("Your screen resolution with " + maxMon + " monitor(s) would be "+ maxRez + " X " + height + " Now that's what I'm talking about! Welcome to the nerd club! WOOTZ WOOTZ!")
     }//print to console
+}
+
+var a = calcRez(1920,1921,3840);//variable to the anonymous function
+console.log (a);//print console
