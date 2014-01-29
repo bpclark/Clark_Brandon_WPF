f57c11717d9d1e32bdaae1ed7e7569e1c77cf831
brandon clark <bpclark@fullsail.edu>
2014-01-28 23:35:08 -0500

    returning back to the previous statement for the callback

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,5 +6,7 @@ Function_Wacky
 
 function theFunction(ducks,goats,turtles)//defining the items that are going to be encapsulated
 {
+    var zoo = ducks + goats + turtles;// giving the calculation needed for the return.
+    return zoo;//returning back to the previous statement for the callback
 
 
