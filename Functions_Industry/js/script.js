ce8bf2f76c56c2fd0008702a12df5a94fc746b72
brandon clark <bpclark@fullsail.edu>
2014-01-28 20:51:15 -0500

    calculation for the maximum screen resolution per added monitor.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -12,3 +12,5 @@ function calcRez(){//normal named function
     var width = prompt ("Enter a single monitors width in pixels (i.e. 1920 1024 ect.)");//width prompt from user
     var height = prompt ("Enter a single monitors height in pixels.(i.e. 480, 720, 1080)");// height prompt from user
     var maxMon = prompt ("Enter in how many monitors you want set up to share a desktop.");// number of monitors the user will be using.
+
+    var maxRez = width* maxMon;// calculation for the maximum screen resolution per added monitor.
