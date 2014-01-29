9b25d7f45d436a48b5ffbb926eb1ffddd9ac73bb
brandon clark <bpclark@fullsail.edu>
2014-01-28 20:49:06 -0500

    width prompt from user

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,3 +9,4 @@ Result: 3 statements depending on resolution.
  */
 
 function calcRez(){//normal named function
+    var width = prompt ("Enter a single monitors width in pixels (i.e. 1920 1024 ect.)");//width prompt from user
