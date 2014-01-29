c91014ea655b55ede4c5c46098bfe62d24dba578
brandon clark <bpclark@fullsail.edu>
2014-01-28 22:28:27 -0500

    user prompt for depth of a desk

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,3 +6,4 @@ Functions_Personal
 
 function deskArea(){//normal function
     var width = prompt ("Enter the width of the desk in inches");//user prompt for width of a desk
+    var depth = prompt ("Enter the depth of the desk in inches");// user prompt for depth of a desk
