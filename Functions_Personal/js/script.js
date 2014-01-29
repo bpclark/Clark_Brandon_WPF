722b12db002d5c622ba0dae6a0642b99d2f3de43
brandon clark <bpclark@fullsail.edu>
2014-01-28 22:33:56 -0500

    missed a }

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -12,8 +12,8 @@ function deskArea(){//normal function
     var result1 = (width * depth) - (fabric *144);// calculation to show how much fabric is needed or not
     if (result1 >0){//condition for the result to print the IF or ELSE
         console.log("You need more fabric! You are short by " + result1+ " square inches.");// print to console
-    else{//begin the else condition if the IF was not met.
-            console.log("You have " + (result1 * -1)  +  " square inches left, so you do not need to buy more fabric")//print to console
+    } else{//begin the else condition if the IF was not met.
+            console.log("You have " + (result1 * -1)  +  " square inches left, so you do not need to buy more fabric");//print to console
         }
     }
     deskArea();// end of normal function
