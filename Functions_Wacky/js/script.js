e205b88a1c2796393f8beb248ef70a3750a8ad19
brandon clark <bpclark@fullsail.edu>
2014-01-28 23:38:32 -0500

    changed a function variable

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -9,5 +9,5 @@ function theFunction(ducks,goats,turtles)//defining the items that are going to 
     var zoo = ducks + goats + turtles;// giving the calculation needed for the return.
     return zoo;//returning back to the previous statement for the callback
 }
-    var animals = theFunction(5, 5, 6);//defining the encapsulated values for the return entries
+    var animals = theFunction(5, 10, 6);//defining the encapsulated values for the return entries
     console.log("There are a total of " + animals + " animals at this petting zoo.");//print to console
