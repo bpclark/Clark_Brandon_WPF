efa676d520ad72e1e0c46de24c55c3c844d9ce4a
brandon clark <bpclark@fullsail.edu>
2014-01-28 23:36:39 -0500

    defining the encapsulated values for the return entries

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -8,5 +8,5 @@ function theFunction(ducks,goats,turtles)//defining the items that are going to 
 {
     var zoo = ducks + goats + turtles;// giving the calculation needed for the return.
     return zoo;//returning back to the previous statement for the callback
-
-
+}
+    var animals = theFunction(5, 5, 6);//defining the encapsulated values for the return entries
